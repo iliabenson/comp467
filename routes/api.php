453ea83ad26729fb16e32c820909bd3877ebe540
Ilia Benson <ilia.benson@yahoo.com>
2017-12-04 09:12:22 +0000

    ready for deployment

--- routes/api.php
+++ routes/api.php
@@ -17,21 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::group(['middleware' => ['cors', 'auth.basic']], function () {
-// 	Route::get('/dashboard', 'HeroController@dash');
-// 	Route::get('/heroes', 'HeroController@index');
-// 	Route::get('/hero/{id}', 'HeroController@show');
-// });
-
 Route::group(['middleware' => 'cors'], function () {
 	Route::get('/settings', 'SettingController@getFirst');
 	Route::put('/settings', 'SettingController@update');
-	Route::get('/dashboard', 'HeroController@dash');
-	Route::delete('/heroes/{id}', 'HeroController@destroy');
-	Route::get('/heroes/search/{name}', 'HeroController@search');
-	Route::get('/heroes/{id}', 'HeroController@show');
-	Route::put('/heroes/{hero}', 'HeroController@update');
-	Route::get('/heroes', 'HeroController@index');
-	Route::post('/heroes', 'HeroController@store');
 });
 
